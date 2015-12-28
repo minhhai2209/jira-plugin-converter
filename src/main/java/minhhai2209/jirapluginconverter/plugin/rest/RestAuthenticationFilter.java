8d435854310eb98a8a16b1f35f3d9a9e0b834f98
Hai M Nguyen <haimnguyen@qasymphony.com>
2015-12-29 01:27:59 +0700

    use atlassian sdk 6.2

--- src/main/java/minhhai2209/jirapluginconverter/plugin/rest/RestAuthenticationFilter.java
+++ src/main/java/minhhai2209/jirapluginconverter/plugin/rest/RestAuthenticationFilter.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.atlassian.crowd.embedded.api.User;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.user.util.UserUtil;
@@ -62,12 +61,10 @@ public class RestAuthenticationFilter implements Filter {
         } else {
 
           UserUtil userUtil = ComponentAccessor.getUserUtil();
-          Collection<User> admins = userUtil.getJiraAdministrators();
-          User admin = Iterables.get(admins, 0);
-          String adminName = admin.getName();
-          ApplicationUser applicationAdmin = userUtil.getUserByName(adminName);
+          Collection<ApplicationUser> admins = userUtil.getJiraAdministrators();
+          ApplicationUser admin = Iterables.get(admins, 0);
           HttpSession httpSession = request.getSession();
-          httpSession.setAttribute(DefaultAuthenticator.LOGGED_IN_KEY, applicationAdmin);
+          httpSession.setAttribute(DefaultAuthenticator.LOGGED_IN_KEY, admin);
           httpSession.setAttribute(DefaultAuthenticator.LOGGED_OUT_KEY, null);
         }
       }
