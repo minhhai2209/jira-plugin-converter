e35f5297ad9fbb8cdfa362e4ba11ba1df57f89d6
Hai M Nguyen <haimnguyen@qasymphony.com>
2016-02-29 14:19:53 +0700

    ignore unknown properties

--- src/main/java/minhhai2209/jirapluginconverter/utils/JsonUtils.java
+++ src/main/java/minhhai2209/jirapluginconverter/utils/JsonUtils.java
@@ -1,13 +1,20 @@
 package minhhai2209.jirapluginconverter.utils;
 
-import java.io.File;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 
 public class JsonUtils {
 
-  private static final ObjectMapper om = new ObjectMapper();
+  private static final ObjectMapper om = getObjectMapper();
+
+  private static ObjectMapper getObjectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    return objectMapper;
+  }
 
   public static <T> T fromJson(String json, Class<T> clazz) {
     try {
