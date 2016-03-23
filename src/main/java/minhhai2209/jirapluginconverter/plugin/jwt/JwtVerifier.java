790a3b72fcd091023ca4a0c2e5e243707c177c97
Likun Liu <lliu@zendesk.com>
2016-03-23 16:50:10 +1100

    fix JWT authentication by processing relativePath using request object to get around the case where JIRA server is running on non default port, also only process JWT from authorisation HEADER, not process JWT params. The request using JWT params won't work.

--- src/main/java/minhhai2209/jirapluginconverter/plugin/jwt/JwtVerifier.java
+++ src/main/java/minhhai2209/jirapluginconverter/plugin/jwt/JwtVerifier.java
@@ -11,27 +11,22 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 
 import com.atlassian.jwt.CanonicalHttpRequest;
-import com.atlassian.jwt.Jwt;
 import com.atlassian.jwt.core.reader.JwtClaimVerifiersBuilder;
 import com.atlassian.jwt.core.reader.JwtIssuerSharedSecretService;
 import com.atlassian.jwt.core.reader.JwtIssuerValidator;
 import com.atlassian.jwt.core.reader.NimbusJwtReaderFactory;
 import com.atlassian.jwt.exception.JwtIssuerLacksSharedSecretException;
 import com.atlassian.jwt.exception.JwtUnknownIssuerException;
-import com.atlassian.jwt.exception.JwtVerificationException;
 import com.atlassian.jwt.reader.JwtClaimVerifier;
 import com.atlassian.jwt.reader.JwtReader;
 import com.atlassian.jwt.reader.JwtReaderFactory;
-
-import minhhai2209.jirapluginconverter.utils.ExceptionUtils;
-import minhhai2209.jirapluginconverter.utils.JsonUtils;
 
 public class JwtVerifier {
 
   private static final int JWT_REALM_LENGTH = "JWT ".length();
 
-  public static JwtClaim read(
-      String url, String authorization, String baseUrl, final String issuer, final String sharedSecret, String method) {
+  public static boolean verify(
+    String relativeURI, String jwtString, Map<String, String[]> parameterMap, final String issuer, final String sharedSecret, String method) {
 
     try {
 
@@ -41,33 +36,15 @@ public class JwtVerifier {
 
       JwtReaderFactory jwtReaderFactory = new NimbusJwtReaderFactory(jwtIssuerValidator, jwtIssuerSharedSecretService);
 
-      final String relativePath = JwtHelper.getRelativePath(url, baseUrl);
-
-      Map<String, List<String>> parameters = getUrlParameters(url);
-
-      final Map<String, String[]> parameterMap = JwtHelper.getParameterMap(parameters);
-
-      Map<String, ? extends JwtClaimVerifier> jwtClaimVerifier = getJwtClaimVerifiers(relativePath, parameterMap, method);
-
-      String jwtString = getJwtToken(authorization, parameters);
-      if (jwtString == null) {
-        return null;
-      }
+      Map<String, ? extends JwtClaimVerifier> jwtClaimVerifier = getJwtClaimVerifiers(relativeURI, parameterMap, method);
 
       JwtReader jwtReader = jwtReaderFactory.getReader(jwtString);
-      Jwt jwt = jwtReader.readAndVerify(jwtString, jwtClaimVerifier);
-
-      String claimJson = jwt.getJsonPayload();
-      JwtClaim claim = JsonUtils.fromJson(claimJson, JwtClaim.class);
-
-      return claim;
-
-    } catch (JwtVerificationException e) {
-      return null;
+      jwtReader.readAndVerify(jwtString, jwtClaimVerifier);
     } catch (Exception e) {
-      ExceptionUtils.throwUnchecked(e);
+      System.out.println("JWT VERIFY ERROR: " + e.getMessage());
+      return false;
     }
-    return null;
+    return true;
   }
 
   private static String getJwtToken(String authorization, Map<String, List<String>> parameters) {
